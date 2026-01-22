508e66343e3d2b88c730454f687b9f63b4f083bf
Ander <cifuentees224@gmail.com>
2026-01-22 11:53:19 -0500

    Add startup script with better error handling

--- dist/src/main.js
+++ dist/src/main.js
@@ -5,9 +5,9 @@ const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors();
-    const port = process.env.PORT || 3000;
-    await app.listen(port);
-    console.log(`Application is running on port ${port}`);
+    const port = process.env.PORT || 10000;
+    await app.listen(port, '0.0.0.0');
+    console.log(`Application is running on http://0.0.0.0:${port}`);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
